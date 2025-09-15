8b8daaa8b5d1ef77cb682a9d9e30fafe3238a25f
sambux1 <sambuxbaum@gmail.com>
2025-09-14 23:14:54 -0400

    prime field seed type for homomorphic prg

--- src/crypto/seed_homomorphic_prg.rs
+++ src/crypto/seed_homomorphic_prg.rs
@@ -1,10 +1,22 @@
+use ark_ff::{Fp, MontBackend, MontConfig};
+
+// create a type for a 128 bit prime field
+#[derive(MontConfig)]
+#[modulus = "170141183460469231731687303715884105727"] // 2^127 - 1
+#[generator = "3"]
+pub struct F128Config;
+pub type F128 = Fp<MontBackend<F128Config, 2>, 2>;
+
 #[derive(Debug)]
 pub struct SeedHomomorphicPRG {
-    seed: Vec<u8>,
+    seed: Vec<Vec<F128>>,
 }
 
 impl SeedHomomorphicPRG {
     pub fn new() -> Self {
-        Self { seed: vec![0, 32] }
+        Self { seed: vec![vec![F128::from(0u64), F128::from(1u64)],
+                          vec![F128::from(2u64), F128::from(3u64)]] }
     }
+
+    pub fn seed(&self) -> &Vec<Vec<F128>> { &self.seed }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use aggregation::crypto::SeedHomomorphicPRG;
 
 fn main() {
-    println!("Hello, world!");
     let prg = SeedHomomorphicPRG::new();
     println!("prg: {:?}", prg);
 }
