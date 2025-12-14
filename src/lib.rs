5de9c60fbbd022d22db2faa11791231b5e27fd7c
sambux1 <sambuxbaum@gmail.com>
2025-12-14 12:11:34 -0500

    create simulator module

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,6 @@ pub mod protocols;
 pub use protocols::{opa, client};
 pub use protocols::{client::Client, server::Server};
 pub use protocols::opa::{OPAClient, OPAServer};
+
+#[cfg(feature = "simulator")]
+pub mod simulator;
---
+++ src/simulator/main.rs
@@ -0,0 +1,6 @@
+use aggregation::opa::OPAClient;
+
+fn main() {
+    let _opa_client = OPAClient::<u32>::new();
+    println!("Simulator starting... (dummy message)");
+}
